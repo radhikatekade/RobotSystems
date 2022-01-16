9d31f9c85db048dc9f19e545fa8ba28b45939723
radhikatekade <tekade.radhika@gmail.com>
2022-01-16 03:32:25 -0800

    add picarx_improved.py

--- lib/picarx_improved.py
+++ lib/picarx_improved.py
@@ -1,8 +1,17 @@
 # from ezblock import Servo,PWM,fileDB,Pin,ADC
 import time
 
+import logging
+from logdecorator import log_on_start , log_on_end , log_on_error
+logger = logging.getlogger()
+logger.setLevel(logging.DEBUG)
+
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s'
+    datefmt='%Y-%m-%d %H:%M:%S')
+
 try:
-    from servo import Servo 
+    from servo import Servo
     from pwm import PWM
     from pin import Pin
     from adc import ADC
@@ -15,6 +24,7 @@ except ImportError:
     print("This computer does not appear to be a PiCar -X system (ezblock is not present). Shadowing hardware calls with substitute functions ")
     from sim_ezblock import*
 
+
 class Picarx(object):
     PERIOD = 4095
     PRESCALER = 10
@@ -25,9 +35,12 @@ class Picarx(object):
         self.camera_servo_pin1 = Servo(PWM('P0'))
         self.camera_servo_pin2 = Servo(PWM('P1'))
         self.config_flie = fileDB('/home/pi/.config')
-        self.dir_cal_value = int(self.config_flie.get("picarx_dir_servo", default_value=0))
-        self.cam_cal_value_1 = int(self.config_flie.get("picarx_cam1_servo", default_value=0))
-        self.cam_cal_value_2 = int(self.config_flie.get("picarx_cam2_servo", default_value=0))
+        self.dir_cal_value = int(self.config_flie.get(
+            "picarx_dir_servo", default_value=0))
+        self.cam_cal_value_1 = int(self.config_flie.get(
+            "picarx_cam1_servo", default_value=0))
+        self.cam_cal_value_2 = int(self.config_flie.get(
+            "picarx_cam2_servo", default_value=0))
         self.dir_servo_pin.angle(self.dir_cal_value)
         self.camera_servo_pin1.angle(self.cam_cal_value_1)
         self.camera_servo_pin2.angle(self.cam_cal_value_2)
@@ -37,25 +50,26 @@ class Picarx(object):
         self.left_rear_dir_pin = Pin("D4")
         self.right_rear_dir_pin = Pin("D5")
 
-
         self.S0 = ADC('A0')
         self.S1 = ADC('A1')
         self.S2 = ADC('A2')
 
-        self.motor_direction_pins = [self.left_rear_dir_pin, self.right_rear_dir_pin]
-        self.motor_speed_pins = [self.left_rear_pwm_pin, self.right_rear_pwm_pin]
-        self.cali_dir_value = self.config_flie.get("picarx_dir_motor", default_value="[1,1]")
-        self.cali_dir_value = [int(i.strip()) for i in self.cali_dir_value.strip("[]").split(",")]
+        self.motor_direction_pins = [
+            self.left_rear_dir_pin, self.right_rear_dir_pin]
+        self.motor_speed_pins = [
+            self.left_rear_pwm_pin, self.right_rear_pwm_pin]
+        self.cali_dir_value = self.config_flie.get(
+            "picarx_dir_motor", default_value="[1,1]")
+        self.cali_dir_value = [int(i.strip())
+                               for i in self.cali_dir_value.strip("[]").split(",")]
         self.cali_speed_value = [0, 0]
         self.dir_current_angle = 0
-        #初始化PWM引脚
+        # 初始化PWM引脚
         for pin in self.motor_speed_pins:
             pin.period(self.PERIOD)
             pin.prescaler(self.PRESCALER)
 
-
-
-    def set_motor_speed(self,motor,speed):
+    def set_motor_speed(self, motor, speed):
         # global cali_speed_value,cali_dir_value
         motor -= 1
         if speed >= 0:
@@ -64,7 +78,7 @@ class Picarx(object):
             direction = -1 * self.cali_dir_value[motor]
         speed = abs(speed)
         if speed != 0:
-            speed = int(speed /2 ) + 50
+            speed = int(speed / 2) + 50
         speed = speed - self.cali_speed_value[motor]
         if direction < 0:
             self.motor_direction_pins[motor].high()
@@ -73,7 +87,7 @@ class Picarx(object):
             self.motor_direction_pins[motor].low()
             self.motor_speed_pins[motor].pulse_width_percent(speed)
 
-    def motor_speed_calibration(self,value):
+    def motor_speed_calibration(self, value):
         # global cali_speed_value,cali_dir_value
         self.cali_speed_value = value
         if value < 0:
@@ -83,7 +97,7 @@ class Picarx(object):
             self.cali_speed_value[0] = abs(self.cali_speed_value)
             self.cali_speed_value[1] = 0
 
-    def motor_direction_calibration(self,motor, value):
+    def motor_direction_calibration(self, motor, value):
         # 0: positive direction
         # 1:negative direction
         # global cali_dir_value
@@ -92,44 +106,43 @@ class Picarx(object):
             self.cali_dir_value[motor] = -1 * self.cali_dir_value[motor]
         self.config_flie.set("picarx_dir_motor", self.cali_dir_value)
 
-
-    def dir_servo_angle_calibration(self,value):
+    def dir_servo_angle_calibration(self, value):
         # global dir_cal_value
         self.dir_cal_value = value
-        print("calibrationdir_cal_value:",self.dir_cal_value)
-        self.config_flie.set("picarx_dir_servo", "%s"%value)
+        print("calibrationdir_cal_value:", self.dir_cal_value)
+        self.config_flie.set("picarx_dir_servo", "%s" % value)
         self.dir_servo_pin.angle(value)
 
-    def set_dir_servo_angle(self,value):
+    def set_dir_servo_angle(self, value):
         # global dir_cal_value
         self.dir_current_angle = value
-        angle_value  = value + self.dir_cal_value
-        print("angle_value:",angle_value)
+        angle_value = value + self.dir_cal_value
+        print("angle_value:", angle_value)
         # print("set_dir_servo_angle_1:",angle_value)
         # print("set_dir_servo_angle_2:",dir_cal_value)
         self.dir_servo_pin.angle(angle_value)
 
-    def camera_servo1_angle_calibration(self,value):
+    def camera_servo1_angle_calibration(self, value):
         # global cam_cal_value_1
         self.cam_cal_value_1 = value
-        self.config_flie.set("picarx_cam1_servo", "%s"%value)
-        print("cam_cal_value_1:",self.cam_cal_value_1)
+        self.config_flie.set("picarx_cam1_servo", "%s" % value)
+        print("cam_cal_value_1:", self.cam_cal_value_1)
         self.camera_servo_pin1.angle(value)
 
-    def camera_servo2_angle_calibration(self,value):
+    def camera_servo2_angle_calibration(self, value):
         # global cam_cal_value_2
         self.cam_cal_value_2 = value
-        self.config_flie.set("picarx_cam2_servo", "%s"%value)
-        print("picarx_cam2_servo:",self.cam_cal_value_2)
+        self.config_flie.set("picarx_cam2_servo", "%s" % value)
+        print("picarx_cam2_servo:", self.cam_cal_value_2)
         self.camera_servo_pin2.angle(value)
 
-    def set_camera_servo1_angle(self,value):
+    def set_camera_servo1_angle(self, value):
         # global cam_cal_value_1
         self.camera_servo_pin1.angle(-1*(value + -1*self.cam_cal_value_1))
         # print("self.cam_cal_value_1:",self.cam_cal_value_1)
         print((value + self.cam_cal_value_1))
 
-    def set_camera_servo2_angle(self,value):
+    def set_camera_servo2_angle(self, value):
         # global cam_cal_value_2
         self.camera_servo_pin2.angle(-1*(value + -1*self.cam_cal_value_2))
         # print("self.cam_cal_value_2:",self.cam_cal_value_2)
@@ -142,55 +155,54 @@ class Picarx(object):
         adc_value_list.append(self.S2.read())
         return adc_value_list
 
-    def set_power(self,speed):
+    def set_power(self, speed):
         self.set_motor_speed(1, speed)
-        self.set_motor_speed(2, speed) 
+        self.set_motor_speed(2, speed)
 
-    def backward(self,speed):
+    def backward(self, speed):
         current_angle = self.dir_current_angle
         if current_angle != 0:
             abs_current_angle = abs(current_angle)
             # if abs_current_angle >= 0:
             if abs_current_angle > 40:
                 abs_current_angle = 40
-            power_scale = (100 - abs_current_angle) / 100.0 
-            print("power_scale:",power_scale)
+            power_scale = (100 - abs_current_angle) / 100.0
+            print("power_scale:", power_scale)
             if (current_angle / abs_current_angle) > 0:
                 self.set_motor_speed(1, -1*speed)
                 self.set_motor_speed(2, speed * power_scale)
             else:
                 self.set_motor_speed(1, -1*speed * power_scale)
-                self.set_motor_speed(2, speed )
+                self.set_motor_speed(2, speed)
         else:
             self.set_motor_speed(1, -1*speed)
-            self.set_motor_speed(2, speed)  
+            self.set_motor_speed(2, speed)
 
-    def forward(self,speed):
+    def forward(self, speed):
         current_angle = self.dir_current_angle
         if current_angle != 0:
             abs_current_angle = abs(current_angle)
             # if abs_current_angle >= 0:
             if abs_current_angle > 40:
                 abs_current_angle = 40
-            power_scale = (100 - abs_current_angle) / 100.0 
-            print("power_scale:",power_scale)
+            power_scale = (100 - abs_current_angle) / 100.0
+            print("power_scale:", power_scale)
             if (current_angle / abs_current_angle) > 0:
                 self.set_motor_speed(1, speed)
                 self.set_motor_speed(2, -1*speed * power_scale)
             else:
                 self.set_motor_speed(1, speed * power_scale)
-                self.set_motor_speed(2, -1*speed )
+                self.set_motor_speed(2, -1*speed)
         else:
             self.set_motor_speed(1, speed)
-            self.set_motor_speed(2, -1*speed)                  
+            self.set_motor_speed(2, -1*speed)
 
     def stop(self):
         self.set_motor_speed(1, 0)
         self.set_motor_speed(2, 0)
 
-
     def Get_distance(self):
-        timeout=0.01
+        timeout = 0.01
         trig = Pin('D8')
         echo = Pin('D9')
 
@@ -202,17 +214,17 @@ class Picarx(object):
         pulse_end = 0
         pulse_start = 0
         timeout_start = time.time()
-        while echo.value()==0:
+        while echo.value() == 0:
             pulse_start = time.time()
             if pulse_start - timeout_start > timeout:
                 return -1
-        while echo.value()==1:
+        while echo.value() == 1:
             pulse_end = time.time()
             if pulse_end - timeout_start > timeout:
                 return -2
         during = pulse_end - pulse_start
         cm = round(during * 340 / 2 * 100, 2)
-        #print(cm)
+        # print(cm)
         return cm
 
 
@@ -232,8 +244,8 @@ if __name__ == "__main__":
 
 # if __name__ == "__main__":
 #     try:
-#         # dir_servo_angle_calibration(-10) 
+#         # dir_servo_angle_calibration(-10)
 #         while 1:
 #             test()
-#     finally: 
+#     finally:
 #         stop()
