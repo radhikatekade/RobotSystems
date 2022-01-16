aa8823cd3b3e90cbbcfe68b14915a0a4d944e0c1
radhikatekade <tekade.radhika@gmail.com>
2022-01-16 01:37:17 -0800

    add parallel parking

--- week2/maneuver.py
+++ week2/maneuver.py
@@ -20,5 +20,69 @@ def forwardBackward():
     finally:
         px.forward(0)
 
+def parallelParking():
+    try:
+        px = Picarx()
+
+        px.forward(30)
+        time.sleep(0.5)
+        
+        px.forward(0)
+        time.sleep(1)
+
+        for angle in range(0,45):
+            px.set_dir_servo_angle(angle)
+            time.sleep(0.01)   
+        
+        px.forward(0)
+        time.sleep(1)
+
+        px.backward(30)
+        time.sleep(0.7)
+        
+        px.forward(0)
+        time.sleep(1)
+
+        for angle in range(45,-45,-1):
+            px.set_dir_servo_angle(angle)
+            time.sleep(0.01)   
+        
+        px.forward(0)
+        time.sleep(1)
+
+        px.backward(20)
+        time.sleep(0.7)
+        
+        px.forward(0)
+        time.sleep(1)
+
+        for angle in range(-45,45):
+            px.set_dir_servo_angle(angle)
+            time.sleep(0.01)   
+    
+        px.forward(0)
+        time.sleep(1)
+        
+        px.forward(30)
+        time.sleep(0.3)   
+
+        px.forward(0)
+        time.sleep(1)
+        
+        for angle in range(45,0,-1):
+            px.set_dir_servo_angle(angle)
+            time.sleep(0.01)
+        
+        px.forward(0)
+        time.sleep(1)
+        
+        px.forward(10)
+        time.sleep(0.2)   
+
+        px.forward(0)
+        time.sleep(1)
+    finally:
+        px.forward(0)
+
 if __name__ == "__main__":
     print("import successful")
