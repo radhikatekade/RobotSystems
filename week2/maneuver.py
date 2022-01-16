b4e91bb5f1d43f0b3f538d92cefa9f4facea4c55
radhikatekade <tekade.radhika@gmail.com>
2022-01-16 01:53:19 -0800

    fix indentation error

--- week2/maneuver.py
+++ week2/maneuver.py
@@ -1,15 +1,15 @@
+import time
+from picarx import Picarx
+from utils import reset_mcu
 import sys
 sys.path.append(r'/home/pi/RobotSystems/lib')
-from utils import reset_mcu
 reset_mcu()
 
-from picarx import Picarx
-import time
 
 def forwardBackward():
     try:
         px = Picarx()
-        
+
         px.forward(30)
         time.sleep(0.5)
         px.backward(30)
@@ -19,6 +19,7 @@ def forwardBackward():
         time.sleep(1)
     finally:
         px.forward(0)
+
 
 def parallelParking():
     try:
@@ -26,58 +27,58 @@ def parallelParking():
 
         px.forward(30)
         time.sleep(0.5)
-        
+
         px.forward(0)
         time.sleep(1)
 
-        for angle in range(0,45):
+        for angle in range(0, 45):
             px.set_dir_servo_angle(angle)
-            time.sleep(0.01)   
-        
+            time.sleep(0.01)
+
         px.forward(0)
         time.sleep(1)
 
         px.backward(30)
         time.sleep(0.7)
-        
+
         px.forward(0)
         time.sleep(1)
 
-        for angle in range(45,-45,-1):
+        for angle in range(45, -45, -1):
             px.set_dir_servo_angle(angle)
-            time.sleep(0.01)   
-        
+            time.sleep(0.01)
+
         px.forward(0)
         time.sleep(1)
 
         px.backward(20)
         time.sleep(0.7)
-        
-        px.forward(0)
-        time.sleep(1)
-
-        for angle in range(-45,45):
-            px.set_dir_servo_angle(angle)
-            time.sleep(0.01)   
-    
-        px.forward(0)
-        time.sleep(1)
-        
-        px.forward(30)
-        time.sleep(0.3)   
 
         px.forward(0)
         time.sleep(1)
-        
-        for angle in range(45,0,-1):
+
+        for angle in range(-45, 45):
             px.set_dir_servo_angle(angle)
             time.sleep(0.01)
-        
+
         px.forward(0)
         time.sleep(1)
-        
+
+        px.forward(30)
+        time.sleep(0.3)
+
+        px.forward(0)
+        time.sleep(1)
+
+        for angle in range(45, 0, -1):
+            px.set_dir_servo_angle(angle)
+            time.sleep(0.01)
+
+        px.forward(0)
+        time.sleep(1)
+
         px.forward(10)
-        time.sleep(0.2)   
+        time.sleep(0.2)
 
         px.forward(0)
         time.sleep(1)
@@ -85,69 +86,70 @@ def parallelParking():
         px.forward(0)
 
     def kTurning():
-    try:
-        px = Picarx()
+        try:
+            px = Picarx()
 
-        px.forward(30)
-        time.sleep(0.5)
-        
-        px.forward(0)
-        time.sleep(1)
+            px.forward(30)
+            time.sleep(0.5)
 
-        for angle in range(0,-45,-1):
-            px.set_dir_servo_angle(angle)
-            time.sleep(0.01)   
-        
-        px.forward(0)
-        time.sleep(1)
+            px.forward(0)
+            time.sleep(1)
 
-        px.forward(30)
-        time.sleep(0.7)
-        
-        px.forward(0)
-        time.sleep(1)
+            for angle in range(0, -45, -1):
+                px.set_dir_servo_angle(angle)
+                time.sleep(0.01)
 
-        for angle in range(-45,45):
-            px.set_dir_servo_angle(angle)
-            time.sleep(0.01)   
-        
-        px.forward(0)
-        time.sleep(1)
+            px.forward(0)
+            time.sleep(1)
 
-        px.backward(30)
-        time.sleep(0.7)
-        
-        px.forward(0)
-        time.sleep(1)
+            px.forward(30)
+            time.sleep(0.7)
 
-        for angle in range(45,-45,-1):
-            px.set_dir_servo_angle(angle)
-            time.sleep(0.01)   
-    
-        px.forward(0)
-        time.sleep(1)
-        
-        px.forward(30)
-        time.sleep(1)   
+            px.forward(0)
+            time.sleep(1)
 
-        px.forward(0)
-        time.sleep(1)
+            for angle in range(-45, 45):
+                px.set_dir_servo_angle(angle)
+                time.sleep(0.01)
 
-        for angle in range(-45,0):
-            px.set_dir_servo_angle(angle)
-            time.sleep(0.01)   
-    
-        px.forward(0)
-        time.sleep(1)
-        
-        px.forward(30)
-        time.sleep(0.7)   
+            px.forward(0)
+            time.sleep(1)
 
-        px.forward(0)
-        time.sleep(1)
+            px.backward(30)
+            time.sleep(0.7)
 
-    finally:
-        px.forward(0)
+            px.forward(0)
+            time.sleep(1)
+
+            for angle in range(45, -45, -1):
+                px.set_dir_servo_angle(angle)
+                time.sleep(0.01)
+
+            px.forward(0)
+            time.sleep(1)
+
+            px.forward(30)
+            time.sleep(1)
+
+            px.forward(0)
+            time.sleep(1)
+
+            for angle in range(-45, 0):
+                px.set_dir_servo_angle(angle)
+                time.sleep(0.01)
+
+            px.forward(0)
+            time.sleep(1)
+
+            px.forward(30)
+            time.sleep(0.7)
+
+            px.forward(0)
+            time.sleep(1)
+
+        finally:
+            px.forward(0)
+
 
 if __name__ == "__main__":
     print("import successful")
