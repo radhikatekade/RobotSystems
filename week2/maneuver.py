5cbb0fc921e7163e4153485f0374984ae92c9d95
radhikatekade <tekade.radhika@gmail.com>
2022-01-16 01:35:58 -0800

    add parallel parking

--- week2/maneuver.py
+++ week2/maneuver.py
@@ -6,6 +6,19 @@ reset_mcu()
 from picarx import Picarx
 import time
 
+def forwardBackward():
+    try:
+        px = Picarx()
+        
+        px.forward(30)
+        time.sleep(0.5)
+        px.backward(30)
+        time.sleep(0.5)
+
+        px.forward(0)
+        time.sleep(1)
+    finally:
+        px.forward(0)
 
 if __name__ == "__main__":
     print("import successful")
