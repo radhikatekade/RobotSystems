0c8c7f6e802a973d4cd27c6bc89ff17f6b87597c
radhikatekade <tekade.radhika@gmail.com>
2022-01-16 01:41:14 -0800

    add k-turning

--- week2/maneuver.py
+++ week2/maneuver.py
@@ -84,5 +84,70 @@ def parallelParking():
     finally:
         px.forward(0)
 
+    def kTurning():
+    try:
+        px = Picarx()
+
+        px.forward(30)
+        time.sleep(0.5)
+        
+        px.forward(0)
+        time.sleep(1)
+
+        for angle in range(0,-45,-1):
+            px.set_dir_servo_angle(angle)
+            time.sleep(0.01)   
+        
+        px.forward(0)
+        time.sleep(1)
+
+        px.forward(30)
+        time.sleep(0.7)
+        
+        px.forward(0)
+        time.sleep(1)
+
+        for angle in range(-45,45):
+            px.set_dir_servo_angle(angle)
+            time.sleep(0.01)   
+        
+        px.forward(0)
+        time.sleep(1)
+
+        px.backward(30)
+        time.sleep(0.7)
+        
+        px.forward(0)
+        time.sleep(1)
+
+        for angle in range(45,-45,-1):
+            px.set_dir_servo_angle(angle)
+            time.sleep(0.01)   
+    
+        px.forward(0)
+        time.sleep(1)
+        
+        px.forward(30)
+        time.sleep(1)   
+
+        px.forward(0)
+        time.sleep(1)
+
+        for angle in range(-45,0):
+            px.set_dir_servo_angle(angle)
+            time.sleep(0.01)   
+    
+        px.forward(0)
+        time.sleep(1)
+        
+        px.forward(30)
+        time.sleep(0.7)   
+
+        px.forward(0)
+        time.sleep(1)
+
+    finally:
+        px.forward(0)
+
 if __name__ == "__main__":
     print("import successful")
